5389d735509a3695565cea500c917dd976b70411
Emerald263 <emeraldminer263@gmail.com>
2025-11-12 12:26:54 -0500

    NataliaLevel

---
+++ Assets/Student Folders/Natalia Jasinski/NJGameManager.cs
@@ -0,0 +1,11 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+//This script handles the game's backend
+//You really shouldn't mess with it
+//Just make sure to set MainNPC and JSON in the editor
+public class NJGameManager : GameManager{ 
+}
